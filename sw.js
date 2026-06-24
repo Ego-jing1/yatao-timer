697525e7b98d13ee7b5537d96f99191140da7782
Ego-jing1 <72849638+Ego-jing1@users.noreply.github.com>
2026-06-24 18:12:26 +0800

    Add files via upload
    新增：
    
    时间显示到秒：HH:MM:SS
    咬胶计时器：1分钟 / 2分钟 / 3分钟
    换牙套倒计时：还剩 X 天 X 小时
    本次摘下时长
    今日摘下次数

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open('aligner-v1').then(c => c.addAll(['./','./index.html','./manifest.json'])));
+  e.waitUntil(caches.open('aligner-v2').then(c => c.addAll(['./','./index.html','./manifest.json'])));
 });
 self.addEventListener('fetch', e => {
   e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
